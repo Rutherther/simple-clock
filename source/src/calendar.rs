b92738752d8b6cc66a2b634b6528a7f5d3f2a315
František Boháček <fandabohacek@gmail.com>
2023-08-06 22:56:54 +0200

    feat(src): add meaningful message to panic in Calendar::days_in_month

--- source/src/calendar.rs
+++ source/src/calendar.rs
@@ -246,7 +246,7 @@ impl Calendar {
             2 => 28,
             1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
             4 | 6 | 9 | 11 => 30,
-            _ => panic!(),
+            _ => panic!("Month {month} does not exist, cannot get days in that month."),
         }
     }
 
